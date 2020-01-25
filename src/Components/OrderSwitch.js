65ae6474f3a514d37abdb82d61e418e725f36e17
Dire <dirediredev@gmail.com>
2020-01-25 12:37:34 +0200

    Added material-ui icons

--- src/Components/OrderSwitch.js
+++ src/Components/OrderSwitch.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import FormGroup from '@material-ui/core/FormGroup';
-import Switch from '@material-ui/core/Switch';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { teal } from '@material-ui/core/colors';
+import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import FormGroup from '@material-ui/core/FormGroup'
+import Switch from '@material-ui/core/Switch'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import { teal } from '@material-ui/core/colors'
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward'
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward'
 
 const OrderBySwitch = withStyles(theme => ({
   switchBase: {
@@ -19,13 +21,13 @@ const OrderBySwitch = withStyles(theme => ({
   thumb: {},
   checked: {},
   track: {},
-}))(Switch);
+}))(Switch)
 
 export default function OrderSwitch(props) {
-  const [state, setState] = React.useState({ascending: false});
+  const [state, setState] = React.useState({ascending: false})
 
   const handleChange = name => event => {
-    setState({ ...state, [name]: event.target.checked });
+    setState({ ...state, [name]: event.target.checked })
     props.handleChange('ascending', event.target.checked)
   };
 
@@ -34,6 +36,7 @@ export default function OrderSwitch(props) {
       <Typography component="div">
         <Grid component="label" container alignItems="center" spacing={1}>
           <Grid item>Descending</Grid>
+          <ArrowDownwardIcon />
           <Grid item>
             <OrderBySwitch
               checked={state.ascending}
@@ -41,6 +44,7 @@ export default function OrderSwitch(props) {
               value="ascending" 
             />
           </Grid>
+          <ArrowUpwardIcon />
           <Grid item>Ascending</Grid>
         </Grid>
       </Typography>
