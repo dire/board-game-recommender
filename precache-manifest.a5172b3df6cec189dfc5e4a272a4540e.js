dd0f491ea62330ee61fe8389eff98428ae57eb86
Dire <dirediredev@gmail.com>
2020-03-14 20:25:19 +0200

    Updates

---
+++ precache-manifest.a5172b3df6cec189dfc5e4a272a4540e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55193d11cdf4bde4b5218b2bb95067a7",
+    "url": "/board-game-recommender/index.html"
+  },
+  {
+    "revision": "d4dbfe701a494d1b6483",
+    "url": "/board-game-recommender/static/css/main.064f90ee.chunk.css"
+  },
+  {
+    "revision": "ca89ba891bf93adeb230",
+    "url": "/board-game-recommender/static/js/2.49872476.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/board-game-recommender/static/js/2.49872476.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4dbfe701a494d1b6483",
+    "url": "/board-game-recommender/static/js/main.e9aabfb8.chunk.js"
+  },
+  {
+    "revision": "638a8d60dce5d7066d2f",
+    "url": "/board-game-recommender/static/js/runtime-main.61c8e5a1.js"
+  }
+]);
