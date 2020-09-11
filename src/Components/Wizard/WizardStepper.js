a507c2094cca4dcdd67812bc05fc3262bfc41b77
Dire <dirediredev@gmail.com>
2020-09-11 13:26:52 +0300

    Box shadow for the stepper.

--- src/Components/Wizard/WizardStepper.js
+++ src/Components/Wizard/WizardStepper.js
@@ -31,6 +31,7 @@ const useStyles = makeStyles(theme => ({
   },
   stepper: {
     backgroundColor: theme.palette.background.paper,
+    boxShadow: "0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)"
   }
 }));
 
